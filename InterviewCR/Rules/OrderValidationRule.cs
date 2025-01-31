26f59b31fb8060cebc19d34f63849d2ce637a264
btjacobson <31464526+btjacobson@users.noreply.github.com>
2025-01-30 18:55:34 -0700

    Create OrderValidationRule.cs

---
+++ InterviewCR/Rules/OrderValidationRule.cs
@@ -0,0 +1,15 @@
+﻿using NRules.Fluent.Dsl;
+
+namespace InterviewCR.Rules
+{
+    public class OrderValidationRule : Rule
+    {
+        public override void Define()
+        {
+            Order order = null;
+            When()
+                .Match<Order>(() => order);
+            Then()
+        }
+    }
+}
